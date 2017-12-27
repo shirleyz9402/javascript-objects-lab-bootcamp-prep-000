3e40246f1802597a1514b7a38859951739f748b3
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-27 22:38:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,{[key]: value})
 }
-function destructivelyUpdateObjectWithKeyandValue(objext,key,value){
- object[key] = value
- return object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ Object[key] = value
+ return Object
 }

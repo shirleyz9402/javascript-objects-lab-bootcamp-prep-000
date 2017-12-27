5052ccdd0cf9f5ddb9857dd443e7354634d65929
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-27 22:49:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  return object
 }
 function deleteFromObjectByKey(object, key) {
-  return delete object[key]
+  var newRecipe = Object.assign({}, object)
+  delete newRecipe[key]
 }

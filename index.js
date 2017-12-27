7be35dd7acbd967a4af84bfc4360c7b0e05ebe0d
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-27 22:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,{[key]: value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- return object[key]=value
+ object[key]=value
+ return object
+  
 }

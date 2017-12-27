0b186f5b13cc76f1b41c1112191972e6a1094a81
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-27 22:44:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ function updateObjectWithKeyAndValue(object,key,value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[key]=value
  return object
-  
+}
+function deleteFromObjectByKey(object, key) {
+  delete 
 }
